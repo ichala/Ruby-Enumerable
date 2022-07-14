f1a4cd1d0f9d8e53e2c518d577c8d16e826ade0f
ichala <alijandhd@gmail.com>
2022-07-14 13:57:08 +0100

    Fix Linters

--- list.rb
+++ list.rb
@@ -1,4 +1,4 @@
-require_relative './my-enumerable'
+require_relative './my_enumerable'
 
 class MyList
   include MyEnumerable
@@ -23,4 +23,4 @@ puts(list.any? { |e| e == 2 })
 puts(list.any? { |e| e == 5 })
 
 # Test #filter
-puts(list.filter { |e| e.even? })
+puts(list.filter(&:even?))
